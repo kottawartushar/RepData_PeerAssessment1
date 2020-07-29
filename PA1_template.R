ff93ceb98daabc6c501dfd4b1a71305195cc03b2
Kottawar <Tushar.Kottawar@Honeywell.com>
2020-07-30 01:23:22 +0530

    updated PA1_tempalate.R and uploaded plot2.png

--- PA1_template.R
+++ PA1_template.R
@@ -15,3 +15,31 @@ hist(sum_steps, xlab = "Total no. of steps", ylab = "Frequency",
 dev.copy(png, file = "plot1.png", width = 480, height = 480)
 
 dev.off()
+
+mean_steps = mean(sum_steps, na.rm = TRUE)
+mean_steps
+median_steps = median(sum_steps, na.rm = TRUE)
+median_steps
+
+## Average daily activity pattern calculation
+
+average_steps <- aggregate(x = list(total_steps = activity_data$steps),
+                           by = list(interval = activity_data$interval), 
+                           FUN = mean, na.rm = TRUE)
+average_steps
+
+plot(average_steps$interval, average_steps$total_steps, xlab = "5-minute interval", 
+     ylab = "Average no. of steps taken" , type = "l")
+
+dev.copy(png, file = "plot2.png", width = 480, height = 480)
+
+dev.off()
+
+max_average <- average_steps[which.max(average_steps$total_steps),]
+max_average
+
+
+
+
+
+
