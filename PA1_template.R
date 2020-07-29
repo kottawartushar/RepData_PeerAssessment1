8852eb5358f287ebcc71038ff1725293161fc233
Kottawar <Tushar.Kottawar@Honeywell.com>
2020-07-30 02:40:11 +0530

    updated PA1_template.R and uploaded plot4.png

--- PA1_template.R
+++ PA1_template.R
@@ -63,10 +63,33 @@ dev.copy(png, file = "plot3.png", width = 480, height = 480)
 
 dev.off()
 
+new_mean_steps = mean(new_sum_steps, na.rm = TRUE)
+new_mean_steps
+new_median_steps = median(new_sum_steps, na.rm = TRUE)
+new_median_steps
 
 
+## Weekdays and Weekend calculation
 
+replaced_values$date <- as.Date(replaced_values$date)
+replaced_values$day <- ifelse(weekdays(replaced_values$date) == "Saturday" | 
+                              weekdays(replaced_values$date) == "Sunday", "weekend", "weekday")
+head(replaced_values)
+summary(replaced_values)
 
+new_average_steps <- aggregate(x = list(total_steps = replaced_values$steps),
+                               by = list(days = replaced_values$day,
+                                         interval = replaced_values$interval), 
+                               FUN = mean, na.rm = TRUE)
+new_average_steps
+
+ggplot(new_average_steps, aes(x = interval, y = total_steps, color = days)) + geom_line() +
+facet_grid(days ~.) + xlab("Average no. of steps taken") + ylab("5-minute interval") +
+ggtitle("Activity pattern comparision between weekdays and weekends")
+
+dev.copy(png, file = "plot4.png", width = 480, height = 480)
+
+dev.off()
 
 
 
